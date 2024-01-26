6c0a44d3d507abdc96b06c3539c050bf5c335a03
im_abramon <shelopugin.vlad@yandex.ru>
2024-01-26 15:27:33 +0300

    Ну это я какие-то копейки получил просто от этого

--- src/js/form.js
+++ src/js/form.js
@@ -1,152 +1,149 @@
 const form = document.querySelector('.form')
-const searchInput = form.querySelector('.form__input');
+const searchInput = form.querySelector('.form__input')
 const datalist = form.querySelector('.form__datalist')
 const repositories = document.querySelector('.list')
 
+let flag = true /* Сомнтительно но окей */
 
-function makeLoading(){
-    const template = listLoadingTemplate.content.firstElementChild;
-    const node = template.cloneNode(true)
-    return node
+const showAutoCompete = () => {
+  datalist.style['display'] = 'block'
 }
 
-
-function makeRepo(item){
-    const name = item.getAttribute("data-name")
-    const owner = item.getAttribute("data-owner")
-    const stars = item.getAttribute("data-stars")
-
-    const node = listItemTemplate.content.firstElementChild.cloneNode(true)
-
-    const nameNode = node.querySelector(".list-item__name")
-    nameNode.innerText = name
-
-    const ownerNode = node.querySelector(".list-item__owner")
-    ownerNode.innerText = owner
-
-    const starsNode = node.querySelector(".list-item__stars")
-    starsNode.innerText = stars
-
-    const button = node.querySelector('.list-item__close-button')
-        button.addEventListener('click', ()=>{
-            // console.log('button repo:', newRepo)
-            repositories.removeChild(node)
-        })
-
-    return node;
+const hideAutoCompete = () => {
+  setTimeout(() => {
+    datalist.style['display'] = 'none'
+  }, 100)
 }
 
-function makeOption(item){
-    const {name, owner, "watchers_count":stars} = item;
-    const {login:ownerName} = owner 
-
-    const node = document.createElement('li');
-
-    node.innerText = name
-    node.setAttribute("data-name", name)
-    node.setAttribute("data-owner", ownerName)
-    node.setAttribute("data-stars", stars)
-
-    node.classList.add("datalist__option")
-
-    node.addEventListener('click', ()=>{
-        const repo = makeRepo(node)
-        searchInput.value = ""
-        repositories.appendChild(repo)
-        datalist.innerHTML = ''
-    })
-
-    return node
+function makeLoading() {
+  const template = listLoadingTemplate.content.firstElementChild
+  const node = template.cloneNode(true)
+  return node
 }
 
-function getSearchURL(query){
-    return `https://api.github.com/search/repositories?q=${encodeURIComponent(query)}&per_page=5`
+function makeRepo(item) {
+  const name = item.getAttribute('data-name')
+  const owner = item.getAttribute('data-owner')
+  const stars = item.getAttribute('data-stars')
+
+  const node = listItemTemplate.content.firstElementChild.cloneNode(true)
+
+  const nameNode = node.querySelector('.list-item__name')
+  nameNode.innerText = name
+
+  const ownerNode = node.querySelector('.list-item__owner')
+  ownerNode.innerText = owner
+
+  const starsNode = node.querySelector('.list-item__stars')
+  starsNode.innerText = stars
+
+  const button = node.querySelector('.list-item__close-button')
+  button.addEventListener('click', () => {
+    // console.log('button repo:', newRepo)
+    repositories.removeChild(node)
+  })
+
+  return node
+}
+
+function makeOption(item) {
+  const { name, owner, 'watchers_count': stars } = item
+  const { login: ownerName } = owner
+
+  const node = document.createElement('li')
+
+  node.innerText = name
+  node.setAttribute('data-name', name)
+  node.setAttribute('data-owner', ownerName)
+  node.setAttribute('data-stars', stars)
+
+  node.classList.add('datalist__option')
+
+  node.addEventListener('click', () => {
+    const repo = makeRepo(node)
+    searchInput.value = ''
+    repositories.appendChild(repo)
+    datalist.innerHTML = ''
+  })
+
+  return node
+}
+
+function makeSorry() {
+  const template = listSorryTemplate.content.firstElementChild
+  const node = template.cloneNode(true)
+  return node
+}
+
+function getSearchURL(query) {
+  console.log(query)
+  return `https://api.github.com/search/repositories?q=${encodeURIComponent(
+    query
+  )}&per_page=5`
 }
 
 const debounce = (fn, debounceTime) => {
-    let timer = undefined;
-    
-    return function(...args){
-        if(timer !== undefined){
-              clearTimeout(timer)
-              timer = setTimeout(()=>{
-               
-                fn.call(this, ...args)
-                clearTimeout(timer)
-                timer = undefined
-               
-              }, debounceTime)
-             
-            return;
-        }
-        
-         clearTimeout(timer)
-         timer = setTimeout(()=>{
-                fn.call(this, ...args)
-                clearTimeout(timer)
-                timer = undefined
-         }, debounceTime)
+  /*Да, мне было страшно, но я это сделал */
+  let timer = undefined
+
+  return function (...args) {
+    if (timer !== undefined) {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        fn.call(this, ...args)
+        clearTimeout(timer)
+        timer = undefined
+      }, debounceTime)
+
+      return
     }
-};
 
-function hardFetch(query, time){
-    if(!time) time = 0;
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn.call(this, ...args)
+      clearTimeout(timer)
+      timer = undefined
+    }, debounceTime)
+  }
+}
 
-    // return fetch(getSearchURL(query))
-    //     .then(response => response.json())
-    //     .catch(()=> hardFetch(query))
-
-    return new Promise((resolve, reject)=>{
-        const timer = setTimeout(()=>{
-            fetch(getSearchURL(query))
-                .then(response => response.json())
-                .catch(()=> hardFetch(query, 500))
-                .then((result=>{
-                    clearTimeout(timer)
-                    resolve(result);
-                }))
-        }, time)
+function getAutoComplete(query) {
+  fetch(getSearchURL(query))
+    .then((response) => {
+      // console.log('ответ сервера', response)
+      return response.json()
     })
+    .then((data) => {
+      // console.log('джсейсон в ответе', data)
+      const items = data.items
+      if (items) {
+        const newItems = items.map(makeOption)
+        datalist.replaceChildren(...newItems)
+        return
+      }
+      flag = false
+      datalist.replaceChildren(makeSorry())
+    })
+    .catch((e) => console.log('Я ошибся! Я могу один раз ошибиться?', e))
 }
 
-function getAutoComplete(query){
-        hardFetch(query)
-        .then(json=>{
-            const items = json.items
-            const newItems = items.map(makeOption)
-            datalist.replaceChildren(... newItems)
-        })
-        .catch(e=>console.log('Ошибка - ', e))
-}
+const getAutoCompleteDebounced = debounce((searchQuery) => {
+  getAutoComplete(searchQuery)
+}, 500)
 
-const autoComplete = debounce(()=>{
-    const searchQuery = searchInput.value;
-    if(searchQuery != ''){
-        datalist.replaceChildren(makeLoading())
+searchInput.addEventListener('input', (evt) => {
+  const value = searchInput.value
+  if (flag) {
+    if (value) {
+      datalist.replaceChildren(makeLoading())
+      getAutoCompleteDebounced(value)
+    } else {
+      datalist.innerHTML = ''
     }
-    getAutoComplete(searchQuery)
-    
-}, 100)
-
-searchInput.addEventListener('input', ()=>{
-    autoComplete()
+  }
 })
 
-searchInput.addEventListener('focus', (evt)=>{
-    if(this.value!=""){
-        datalist.style['display'] = "block"
-    }else{}
-    
-})
+// Я — великий грешник
 
-searchInput.addEventListener('blur', ()=>{
-    const searchQuery = searchInput.value;
-    setTimeout(()=>{
-        datalist.style['display'] = "none"
-        // if(searchQuery == ''){
-        //     datalist.innerHTML = ''
-        //     datalist.replaceChildren(makeLoading())
-        // }
-    }, 100)
-    
-})
+searchInput.addEventListener('focus', showAutoCompete)
+searchInput.addEventListener('blur', hideAutoCompete)
